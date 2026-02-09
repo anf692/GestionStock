a3f55396b6761f6f208ac882c3b653b1e98f2acc
anf692 <fassane348@gmail.com>
2026-02-09 15:21:19 +0000

    ajoute de la fonction fermeture et de la fonction principal

--- brief5.sql
+++ brief5.sql
@@ -1,2 +1,35 @@
 create database gestion_stock;
 use gestion_stock;
+
+
+CREATE TABLE categories (
+    id_categorie INT AUTO_INCREMENT PRIMARY KEY,
+    nom_categorie VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE produits (
+    id_produit INT AUTO_INCREMENT PRIMARY KEY,
+    designation VARCHAR(150) NOT NULL,
+    prix_unitaire DECIMAL(10,2) NOT NULL,
+    quantite_stock INT NOT NULL DEFAULT 0,
+    en_rupture BOOLEAN NOT NULL DEFAULT FALSE,
+    id_categorie INT NOT NULL,
+
+    CONSTRAINT fk_produit_categorie
+        FOREIGN KEY (id_categorie)
+        REFERENCES categories(id_categorie)
+);
+
+
+CREATE TABLE mouvements (
+    id_mouvement INT AUTO_INCREMENT PRIMARY KEY,
+    id_produit INT NOT NULL,
+    type_mouvement ENUM('ENTREE', 'SORTIE') NOT NULL,
+    quantite INT NOT NULL,
+    date_mouvement DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_mouvement_produit
+        FOREIGN KEY (id_produit)
+        REFERENCES produits(id_produit)
+);
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ connexion = mysql.connector.connect(
     host="localhost",
     user="root", 
     password="tresbienmerci",
-    database="GestionStock"
+    database="gestion_stock"
 )
 
 # Vérifiez si la connexion est réussie
@@ -20,7 +20,13 @@ curseur = connexion.cursor()
 
 #fonction pour ajouter un categorie
 def ajouter_categorie():
-    nom= input("Entrer votre nom: ")
+    while True:
+        nom = input("Entrer le nom de la categorie : ").strip()
+        if nom.replace(" ","").isalpha():
+            break
+        else:
+            print("Incorrect ! le nom de la categorie (seulement des lettres).")
+
     sql = "INSERT INTO categories (nom_categorie) VALUES (%s)"
     curseur.execute(sql, (nom,))
     connexion.commit()
@@ -34,3 +40,35 @@ def lister_categories():
     for id_cat, nom in curseur.fetchall():
         print(f"{id_cat} - {nom}")
    
+
+
+#fonction pour fermer la base de donnnees
+def fermeture():
+    if connexion.is_connected():
+        connexion.close()
+        print("Connexion à la base de données MySQL fermée.")
+
+
+#fonction principal
+def menu():
+    while True:
+        print("\n--- Menu ---")
+        print("1. Ajouter un categorie")
+        print("2. Lister categorie")
+        print("3. Afficher présents")
+        print("4. Rechercher apprenant")
+        print("5. Supprimer un apprenant")
+        print("6. Quitter")
+        choix = input("Votre choix : ")
+        if choix == "1":
+            ajouter_categorie()
+        elif choix == "2":
+            lister_categories()
+        elif choix == "6":
+            fermeture()
+            print("Au revoir 👋")
+            break
+        else:
+            print("Choix invalide, réessayez.")
+
+menu()
